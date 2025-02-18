33803d94b4c7c366fa189f1a2ab57b5956a81a66
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2025-02-19 02:16:01 +0700

    don't show comment on frontpage

--- src/theme/BlogPostItem/index.tsx
+++ src/theme/BlogPostItem/index.tsx
@@ -3,14 +3,18 @@ import BlogPostItem from '@theme-original/BlogPostItem';
 import type BlogPostItemType from '@theme/BlogPostItem';
 import type {WrapperProps} from '@docusaurus/types';
 import Comments from '@site/src/components/Comments';
+import { useBlogPost } from "@docusaurus/plugin-content-blog/client";
 
 type Props = WrapperProps<typeof BlogPostItemType>;
 
 export default function BlogPostItemWrapper(props: Props): ReactNode {
+  const { metadata, isBlogPostPage } = useBlogPost();
+  const { comments = true } = metadata.frontMatter;
+
   return (
     <>
       <BlogPostItem {...props} />
-      <Comments />
+      {comments && isBlogPostPage && <Comments />}
     </>
   );
 }
