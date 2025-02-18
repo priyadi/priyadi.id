d71553026e1ceff9a9d7d3b874d4a21cfeffefcd
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2025-02-18 23:48:24 +0700

    komeng

--- src/theme/BlogPostItem/index.tsx
+++ src/theme/BlogPostItem/index.tsx
@@ -2,6 +2,7 @@ import React, {type ReactNode} from 'react';
 import BlogPostItem from '@theme-original/BlogPostItem';
 import type BlogPostItemType from '@theme/BlogPostItem';
 import type {WrapperProps} from '@docusaurus/types';
+import Comments from '@site/src/components/Comments';
 
 type Props = WrapperProps<typeof BlogPostItemType>;
 
@@ -9,21 +10,7 @@ export default function BlogPostItemWrapper(props: Props): ReactNode {
   return (
     <>
       <BlogPostItem {...props} />
-      <script src="https://giscus.app/client.js"
-        data-repo="priyadi/priyadi.id"
-        data-repo-id="R_kgDON7bvLg"
-        data-category="Blog Comments"
-        data-category-id="DIC_kwDON7bvLs4CnEzv"
-        data-mapping="pathname"
-        data-strict="1"
-        data-reactions-enabled="1"
-        data-emit-metadata="0"
-        data-input-position="top"
-        data-theme="preferred_color_scheme"
-        data-lang="id"
-        crossorigin="anonymous"
-        async>
-      </script>
+      <Comments />
     </>
   );
 }
